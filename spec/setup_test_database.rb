bec037d84b8ee88c9761df0a3cc311ce2bff567c
ian k <kickthechair@gmail.com>
2021-08-17 16:54:11 +0100

    updated README for test database instructions. Refactored tests

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -1,7 +1,9 @@
 require 'pg'
 
-con = PG.connect :dbname => 'bookmark_manager_test'
-con.exec "TRUNCATE TABLE bookmarks"
-con.exec "INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com/')"
-con.exec "INSERT INTO bookmarks (url) VALUES ('http://www.google.com/')"
-con.exec "INSERT INTO bookmarks (url) VALUES ('http://www.destroyallsoftware.com')"
+def setup_test_database
+  con = PG.connect :dbname => 'bookmark_manager_test'
+  con.exec "TRUNCATE TABLE bookmarks"
+  con.exec "INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com/')"
+  con.exec "INSERT INTO bookmarks (url) VALUES ('http://www.google.com/')"
+  con.exec "INSERT INTO bookmarks (url) VALUES ('http://www.destroyallsoftware.com')"
+end
