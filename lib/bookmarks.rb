d25c81c94e2104cd297b608ba54e69c7756115f6
danroche10 <danielroche10@gmail.com>
2021-08-19 15:28:31 +0100

    can add new bookmark by creating new instance of Bookmarks class

--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -2,6 +2,14 @@ require 'pg'
 
 class Bookmarks
 
+  attr_reader :id, :title, :url
+
+  def initialize(id:, title:, url:)
+    @id = id
+    @title = title
+    @url = url
+  end
+
   def self.all
     if ENV['RACK_ENV'] == 'test'
       con = PG.connect :dbname => 'bookmark_manager_test'
@@ -11,7 +19,7 @@ class Bookmarks
     
     rs = con.exec('SELECT * FROM bookmarks')
     rs.map do |row| 
-      row['title']
+      Bookmarks.new(id: row['id'], title: row['title'], url: row['url'])
     end
   end
 
@@ -22,6 +30,7 @@ class Bookmarks
       con = PG.connect :dbname => 'bookmark_manager'
     end
     
-    rs = con.exec "INSERT INTO bookmarks (url, title) VALUES ('#{url}', '#{title}')"
+    rs = con.exec "INSERT INTO bookmarks (url, title) VALUES ('#{url}', '#{title}') RETURNING id, url, title"
+    Bookmarks.new(id: rs[0]['id'], title: rs[0]['title'], url: rs[0]['url'])
   end
 end
